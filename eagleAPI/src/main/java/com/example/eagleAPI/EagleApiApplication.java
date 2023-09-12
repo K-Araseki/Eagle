d69418ed899c9ff953c200db1a398a7a5f95d642
arabiki <arabiki330@gmail.com>
2023-09-12 18:31:31 +0900

    DB接続用のシステムを追加

---
+++ eagleAPI/src/main/java/com/example/eagleAPI/EagleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eagleAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleApiApplication.class, args);
+	}
+
+}
---
+++ eagleAPI/src/test/java/com/example/eagleAPI/EagleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eagleAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
