19c8b960f4839ffeedd40abfc9d600ef1f1458a7
DESKTOP-N2G70IE\techitose <kklab.unity.mr@gmail.com>
2023-10-25 19:22:09 +0900

    ReadQRViewをtextFieldで代用

---
+++ attendanceManagement/src/main/java/org/vaadin/example/view/ReadQRcodeView.java
@@ -0,0 +1,47 @@
+package org.vaadin.example.view;
+
+import com.vaadin.base.devserver.BrowserLauncher;
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.internal.BrowserLiveReload;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+
+@Route("readQR")
+//@JsModule("https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js")
+public class ReadQRcodeView extends Div {
+
+    private TextField textField;
+    private Button button;
+
+    public ReadQRcodeView() {
+
+        textField = new TextField("社員ID");
+        button = new Button("送信");
+
+        HorizontalLayout horizontalLayout =new HorizontalLayout(textField, button);
+        horizontalLayout.setSizeFull();
+        horizontalLayout.setPadding(true);
+        horizontalLayout.setAlignItems(FlexComponent.Alignment.END);
+        horizontalLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        add(horizontalLayout);
+
+        button.addClickListener(e -> {
+            if(textField.getValue().equals("A001")){
+                System.out.println("A001です");
+                button.getUI().ifPresent(ui -> ui.navigate());
+            }
+            else {
+                System.out.println("その他です");
+            }
+        });
+
+
+    }
+}
