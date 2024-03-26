05b802649972bd2014d3ba50e2e89ad24a6fd35c
DESKTOP-N2G70IE\techitose <kklab.unity.mr@gmail.com>
2024-03-26 18:19:55 +0900

    SelectRegistrationOrModificationViewの文字位置変更

--- attendanceManagement/src/main/java/org/vaadin/example/view/SelectRegistrationOrModificationView.java
+++ attendanceManagement/src/main/java/org/vaadin/example/view/SelectRegistrationOrModificationView.java
@@ -1,17 +1,23 @@
 package org.vaadin.example.view;
 
+import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
 
 @Route(value = "SelectRegistrationOrModification", layout = MainView.class)
-public class SelectRegistrationOrModificationView extends Div {
+public class SelectRegistrationOrModificationView extends Composite<VerticalLayout> {
 
     public SelectRegistrationOrModificationView() {
 
@@ -53,7 +59,7 @@ public class SelectRegistrationOrModificationView extends Div {
 //        HorizontalLayout horizontalLayout1 = new HorizontalLayout(button1, button2);
 //        horizontalLayout1.setAlignItems(Alignment.CENTER);
 //        verticalLayout.add(horizontalLayout1);
-
+/*
         setSizeFull();
 
         H1 text = new H1("勤怠入力");
@@ -72,10 +78,41 @@ public class SelectRegistrationOrModificationView extends Div {
         //水平方向のセンタリング
         horizontalLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
 
-        add(text, horizontalLayout);
+        add(text, horizontalLayout);*/
 
-        button1.addClickListener(buttonClickEvent -> {
-            button1.getUI().ifPresent(ui -> ui.navigate(ReadQRcodeView.class));
+        H1 h1 = new H1();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        Button buttonPrimary = new Button();
+        Button buttonPrimary2 = new Button();
+        getContent().addClassName(Padding.XLARGE);
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        getContent().setJustifyContentMode(JustifyContentMode.START);
+        getContent().setAlignItems(Alignment.CENTER);
+        h1.setText("勤怠入力");
+        getContent().setAlignSelf(FlexComponent.Alignment.CENTER, h1);
+        h1.setWidth("max-content");
+        layoutRow.setWidthFull();
+        getContent().setFlexGrow(1.0, layoutRow);
+        layoutRow.addClassName(Gap.XLARGE);
+        layoutRow.addClassName(Padding.XLARGE);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutRow.setAlignItems(Alignment.START);
+        layoutRow.setJustifyContentMode(JustifyContentMode.CENTER);
+        buttonPrimary.setText("登録");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonPrimary2.setText("修正");
+        buttonPrimary2.setWidth("min-content");
+        buttonPrimary2.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        getContent().add(h1);
+        getContent().add(layoutRow);
+        layoutRow.add(buttonPrimary);
+        layoutRow.add(buttonPrimary2);
+
+        buttonPrimary.addClickListener(buttonClickEvent -> {
+            buttonPrimary.getUI().ifPresent(ui -> ui.navigate(ReadQRcodeView.class));
         });
 
     }
